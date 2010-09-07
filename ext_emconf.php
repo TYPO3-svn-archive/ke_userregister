b8370e957ab197c4db7a1616cb15a608b32e0279
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-07 13:12:25 +0000

    TER v0.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_userregister/trunk@37771 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "ke_userregister"
+# Extension Manager/Repository config file for ext "ke_userregister".
 #
-# Auto generated 22-09-2009 11:31
+# Auto generated 03-09-2010 12:50
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'www.kennziffer.com GmbH',
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,6 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"335e";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"8ca4";s:17:"ext_localconf.php";s:4:"941c";s:14:"ext_tables.php";s:4:"81e9";s:14:"ext_tables.sql";s:4:"1acf";s:19:"ext_tables_edit.php";s:4:"a08c";s:19:"ext_tables_edit.sql";s:4:"70f5";s:15:"flexform_ds.xml";s:4:"2f5d";s:16:"locallang_db.xml";s:4:"050c";s:7:"tca.php";s:4:"8105";s:14:"doc/manual.sxw";s:4:"b9b7";s:19:"doc/wizard_form.dat";s:4:"32ee";s:20:"doc/wizard_form.html";s:4:"2c3f";s:42:"lib/class.tx_keuserregister_cms_layout.php";s:4:"7d9c";s:35:"lib/class.tx_keuserregister_lib.php";s:4:"602c";s:18:"pi1/_locallang.xml";s:4:"8edb";s:35:"pi1/class.tx_keuserregister_pi1.php";s:4:"b3ef";s:17:"pi1/locallang.xml";s:4:"6a52";s:15:"pi1/pi_icon.gif";s:4:"0552";s:35:"pi2/class.tx_keuserregister_pi2.php";s:4:"3415";s:17:"pi2/locallang.xml";s:4:"4e92";s:15:"pi2/pi_icon.gif";s:4:"05fb";s:35:"pi3/class.tx_keuserregister_pi3.php";s:4:"1e20";s:17:"pi3/locallang.xml";s:4:"18f0";s:15:"pi3/pi_icon.gif";s:4:"e78a";s:27:"res/css/ke_userregister.css";s:4:"5113";s:35:"res/template/tx_keuserregister.tmpl";s:4:"0b7c";s:23:"static/ts/constants.txt";s:4:"d41d";s:19:"static/ts/setup.txt";s:4:"b443";}',
 );
 
 ?>
