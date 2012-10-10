516addc2d4aa980d3b52adf9ba0c7172cf2061e5
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-10 10:42:46 +0000

    updated ext_emconf after ter release 0.1.4
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_userregister/trunk@66809 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ke_userregister".
 #
-# Auto generated 09-10-2012 16:46
+# Auto generated 10-10-2012 12:41
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'www.kennziffer.com GmbH',
-	'version' => '0.1.3',
+	'version' => '0.1.4',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"2b76";s:10:"README.txt";s:4:"ee2d";s:22:"birthdayFields.php.inc";s:4:"3aba";s:12:"ext_icon.gif";s:4:"8ca4";s:17:"ext_localconf.php";s:4:"963d";s:14:"ext_tables.php";s:4:"d25e";s:14:"ext_tables.sql";s:4:"50e0";s:15:"flexform_ds.xml";s:4:"2f5d";s:16:"locallang_db.xml";s:4:"e34d";s:7:"tca.php";s:4:"8105";s:14:"doc/manual.sxw";s:4:"4b3a";s:19:"doc/wizard_form.dat";s:4:"32ee";s:20:"doc/wizard_form.html";s:4:"2c3f";s:42:"lib/class.tx_keuserregister_cms_layout.php";s:4:"7d9c";s:35:"lib/class.tx_keuserregister_lib.php";s:4:"55a0";s:35:"pi1/class.tx_keuserregister_pi1.php";s:4:"df89";s:17:"pi1/locallang.xml";s:4:"3713";s:15:"pi1/pi_icon.gif";s:4:"0552";s:35:"pi2/class.tx_keuserregister_pi2.php";s:4:"e982";s:17:"pi2/locallang.xml";s:4:"69e4";s:15:"pi2/pi_icon.gif";s:4:"05fb";s:35:"pi3/class.tx_keuserregister_pi3.php";s:4:"79a4";s:17:"pi3/locallang.xml";s:4:"18f0";s:15:"pi3/pi_icon.gif";s:4:"e78a";s:27:"res/css/ke_userregister.css";s:4:"9fff";s:25:"res/scripts/RemoveXSS.php";s:4:"4252";s:35:"res/template/tx_keuserregister.tmpl";s:4:"03d7";s:23:"static/ts/constants.txt";s:4:"d41d";s:19:"static/ts/setup.txt";s:4:"3408";}',
+	'_md5_values_when_last_written' => 'a:29:{s:9:"ChangeLog";s:4:"1173";s:10:"README.txt";s:4:"ee2d";s:22:"birthdayFields.php.inc";s:4:"3aba";s:12:"ext_icon.gif";s:4:"8ca4";s:17:"ext_localconf.php";s:4:"963d";s:14:"ext_tables.php";s:4:"d25e";s:14:"ext_tables.sql";s:4:"50e0";s:15:"flexform_ds.xml";s:4:"2f5d";s:16:"locallang_db.xml";s:4:"e34d";s:7:"tca.php";s:4:"8105";s:14:"doc/manual.sxw";s:4:"ec93";s:19:"doc/wizard_form.dat";s:4:"32ee";s:20:"doc/wizard_form.html";s:4:"2c3f";s:42:"lib/class.tx_keuserregister_cms_layout.php";s:4:"7d9c";s:35:"lib/class.tx_keuserregister_lib.php";s:4:"55a0";s:35:"pi1/class.tx_keuserregister_pi1.php";s:4:"df89";s:17:"pi1/locallang.xml";s:4:"3713";s:15:"pi1/pi_icon.gif";s:4:"0552";s:35:"pi2/class.tx_keuserregister_pi2.php";s:4:"e982";s:17:"pi2/locallang.xml";s:4:"69e4";s:15:"pi2/pi_icon.gif";s:4:"05fb";s:35:"pi3/class.tx_keuserregister_pi3.php";s:4:"79a4";s:17:"pi3/locallang.xml";s:4:"18f0";s:15:"pi3/pi_icon.gif";s:4:"e78a";s:27:"res/css/ke_userregister.css";s:4:"9fff";s:25:"res/scripts/RemoveXSS.php";s:4:"4252";s:35:"res/template/tx_keuserregister.tmpl";s:4:"03d7";s:23:"static/ts/constants.txt";s:4:"d41d";s:19:"static/ts/setup.txt";s:4:"3408";}',
 );
 
 ?>
