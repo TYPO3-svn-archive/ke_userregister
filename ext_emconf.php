a740d7d876ce22d2a78407aa063ef4fd70c1b869
kiefer <kiefer@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-09-16 13:30:25 +0000

    TYPO3 dependency settings updated
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ke_userregister/trunk@79802 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,11 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.4',
 	'constraints' => array(
 		'depends' => array(
-			'constraints' => array(
-				'depends' => array(
-					'typo3' => '4.5.0-6.1.99',
-			),
-		),
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
